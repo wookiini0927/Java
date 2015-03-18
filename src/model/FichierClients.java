d24e39dbc965b66be19a85018f8541c9092a278b
tiffie27 <lim.steffie@gmail.com>
2015-03-18 18:11:15 +0100

    constructeur + fonction loadfile

--- src/model/FichierClients.java
+++ src/model/FichierClients.java
@@ -12,11 +12,25 @@ import java.util.Scanner;
  *
  */
 public class FichierClients {
-		
+	ArrayList<Client> liste_client;
 	public FichierClients(){
-		
+		liste_client = new ArrayList<Client>();
 	}
 	
+	/**
+	 * @return the liste_client
+	 */
+	public ArrayList<Client> getListe_client() {
+		return liste_client;
+	}
+
+	/**
+	 * @param liste_client the liste_client to set
+	 */
+	public void setListe_client(ArrayList<Client> liste_client) {
+		this.liste_client = liste_client;
+	}
+
 	/**
 	 * Verifie si le numero client n'existe pas deja
 	 * Compare le num au numeroclient de chaque client dans c
@@ -60,8 +74,8 @@ public class FichierClients {
 	 * @param fileName
 	 * @return un tableau de client
 	 */
-	public ArrayList<Client> loadFile(String fileName){
-		ArrayList<Client> c = new ArrayList<Client>();
+	public FichierClients loadFile(String fileName){
+		FichierClients c = new FichierClients();
 		InputStream is = null;
 		Scanner sc = null;
 		String line;
@@ -80,7 +94,7 @@ public class FichierClients {
 				line = sc.nextLine();
 				words = line.split(";");
 				if(words.length == 4){
-					exist_deja = verification(c, words[0]);
+					exist_deja = c.verification(c.liste_client, words[0]);
 					for (int i = 0; i<4; i++){
 						//boucle qui permet de convertir le string en int pour le numeroClient
 						for(int k = 0; k<words[0].length();k++){
@@ -94,7 +108,7 @@ public class FichierClients {
 					}
 
 					if(exist_deja == false){
-						c.add(new Client(words[2],words[3], nClient, code));
+						c.liste_client.add(new Client(words[2],words[3], nClient, code));
 					}
 					/*
 					 * else{
@@ -114,6 +128,47 @@ public class FichierClients {
 			return null;
 		}
 		return c;
+	}
+	
+	/**
+	 * 
+	 * fonction qui chercher s'il ya un match lorsd e la connexion
+	 * @param fileName
+	 * @return true s'il y a un match, sinon false
+	 */
+	public boolean loadConnexion(String fileName, String nc, String pwd, String[] info){
+		InputStream is = null;
+		Scanner sc = null;
+		String line;
+		String[] words;
+		
+		boolean res = false;
+		
+		try{
+			is = FichierClients.class.getClassLoader().getResourceAsStream(fileName);
+			sc = new Scanner(is);
+			while(sc.hasNext() && res == false){
+				line = sc.nextLine();
+				words = line.split(";");
+				if(words[0].equals(nc) && words[1].equals(pwd)){
+					res = true;
+					for(int i = 0;i<words.length;i++){
+						info[i] = words[i];
+					}
+				}
+				else{
+					res = false;
+				}				
+			}
+			sc.close();
+		}
+		catch(Exception e){
+			System.err.println("Error:"+e.getMessage());
+			if(sc!=null){
+				sc.close();
+			}
+		}
+		return res;
 	}
 
 	
