4ad5a59a36b7c29983425547637f7430e07e7e0d
tiffie27 <lim.steffie@gmail.com>
2015-03-18 22:16:50 +0100

    radiobutton bug

--- src/view/FenetreVirement.java
+++ src/view/FenetreVirement.java
@@ -6,6 +6,7 @@ package view;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 
@@ -37,10 +38,12 @@ public class FenetreVirement extends JPanel {
 	private Fenetre fen;
 	private JPanel panTotal; //panel de tous les elements
 	private JPanel panInfo; //panel de gauche info du client et divers options
-	private JPanel panComptes; //panel total des comptes virement et du client
+	private JPanel panResume; //panCompte+panVirement
+	private JPanel panComptes; //panel des comptes client et beneficiaire
 	private JPanel panVirement; //panel de questionnaire du virement
-	private JPanel panLib;
-	private JPanel panResume;
+	private JPanel panLib, panChampLib;
+	private JPanel panMontant, panMontLib;
+	private JPanel panLabDate, panRadioBut;
 	private JLabel num_client, nom_client, prenom_client;
 	private JLabel compteDeb, compteVir;
 	private JLabel libelle_virement, labMontant, lab_date_Virement, lab_euro;
@@ -63,32 +66,43 @@ public class FenetreVirement extends JPanel {
 		benef = new FichierComptes();
 		benef = FichierComptes.loadFile("Doc/CompteBeneficiaire"+numero+".csv");
 		
+		panTotal = new JPanel();
+
+		panInfo = new JPanel();
+
 		num_client = new JLabel(info[0]);
 		nom_client = new JLabel(info[2]);
 		prenom_client = new JLabel(info[3]);
 		newCompte = new JButton(/*new createCompte(*/"Ajouter Compte")/*)*/;
 
-		panTotal = new JPanel();
 		//panTotal.setLayout(fen.getCl());
 		
-		panInfo = new JPanel();
-		num_client = new JLabel(numero);
-		
+//		num_client = new JLabel(numero);
 		
 		//panel compte + versement
 		panResume = new JPanel();
+		
 		panComptes = new JPanel();
+		
 		compteDeb = new JLabel("Compte a debiter");
 		compteVir = new JLabel("Compte a crediter");
 		comptePerso = new JComboBox<String>(FichierComptes.initComptesPerso(fc.getListe_comptes()));
 		compteBeneficiaire = new JComboBox<String>(FichierComptes.initComptesPerso(benef.getListe_comptes()));
 		
 		panVirement = new JPanel();
+		
 		panLib = new JPanel();
-		libelle_virement = new JLabel("Libelle");
-		labMontant = new JLabel("Montant : ");
-		lab_date_Virement = new JLabel("Date d'effet du virement");
+		panChampLib = new JPanel();
+		panMontant = new JPanel();
+		panMontLib = new JPanel();
+		panLabDate = new JPanel();
+		panRadioBut = new JPanel();
+		
+		libelle_virement = new JLabel("Libelle ");
+		labMontant = new JLabel("Montant ");
+		lab_date_Virement = new JLabel("Date d'effet du virement ");
 		lab_euro = new JLabel("EUR");
+		
 		champMontant = new JTextField("",10);
 		champ_libelle = new JTextField("",10);
 
@@ -110,60 +124,71 @@ public class FenetreVirement extends JPanel {
 		Dimension taille_panVir = new Dimension(450,200);
 		
 		panTotal.setBackground(Color.red);
+		
 		//Panel d'info sur le client a gauche
-		panInfo.add(nom_client);
-		panInfo.add(prenom_client);
-		panInfo.add(newCompte);
 		panInfo.setPreferredSize(taille_info);
 		panInfo.setBackground(Color.WHITE);
 		panInfo.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
 		
+		panInfo.add(nom_client);
+		panInfo.add(prenom_client);
+		panInfo.add(newCompte);
+		
 		
 		panResume.setLayout(new GridLayout(2,1));
+		
 		//Panel des resumes de comptes et preference benificiaire
 		panComptes.setPreferredSize(taille_panComptes);
 		panComptes.setLayout(new GridLayout(2,2));
 		panComptes.setBorder(BorderFactory.createLineBorder(Color.BLUE, 2));
 		
-		
 		comptePerso.setEditable(false);
 		compteBeneficiaire.setEditable(false);
-		//comptePerso.setBorder(BorderFactory.createLineBorder(Color.black, 1));
-		//compteBeneficiaire.setBorder(BorderFactory.createLineBorder(Color.black, 1));
 		
 		panComptes.add(compteDeb);
 		panComptes.add(compteVir);
 		panComptes.add(comptePerso);
 		panComptes.add(compteBeneficiaire);
 		
-		//GridLayout gl = new GridLayout(3,2);
+		GridLayout gl = new GridLayout(3,2);
 		panVirement.setPreferredSize(taille_panVir);
-		panVirement.setLayout(new GridBagLayout());
+		panVirement.setLayout(gl);
 		panVirement.setBorder(BorderFactory.createLineBorder(Color.BLACK,1));
-		//panVirement.add(libelle_virement);
-		
-		panVirement.setLayout(new GridBagLayout());
 		panVirement.setBackground(Color.GREEN);
-		panVirement.add(libelle_virement);
-		panVirement.add(champ_libelle);		
-		//panVirement.add(panLib);
-		
-		panVirement.add(labMontant);
-		panVirement.add(lab_euro);
 
-		panVirement.add(champMontant);
-		panVirement.add(lab_date_Virement);
+		FlowLayout align_gauche = new FlowLayout(FlowLayout.LEFT);
+		FlowLayout align_droit = new FlowLayout(FlowLayout.RIGHT);
+
+		panLib.setLayout(align_droit);
+		panLib.add(libelle_virement);
+		panChampLib.setLayout(align_gauche);
+		panChampLib.add(champ_libelle);	
+		
+		panMontant.setLayout(align_droit);
+		panMontant.add(labMontant);
+		panMontLib.setLayout(align_gauche);
+		panMontLib.add(champMontant);
+		panMontLib.add(lab_euro);	
+		
+		panLabDate.setLayout(align_droit);
+		panLabDate.add(lab_date_Virement);
 		
 		immediat = new JRadioButton("Immediat");
 		immediat.setSelected(true);
 		different = new JRadioButton("Differe");
-		
 		ButtonGroup bg = new ButtonGroup();
 		bg.add(immediat);
 		bg.add(different);
 		
-		/*panVirement.add(immediat);
-		panVirement.add(different);*/
+		panRadioBut.add(immediat);
+		panRadioBut.add(different);
+		
+		panVirement.add(panLib);
+		panVirement.add(panChampLib);
+		panVirement.add(panMontant);
+		panVirement.add(panMontLib);
+		panVirement.add(panLabDate);
+		panVirement.add(panRadioBut);
 
 		panResume.add(panComptes);
 		panResume.add(panVirement);
